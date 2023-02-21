47e6d1012cbd9cc8037662aa67b714680419c98d
Boris Wilhelms <mail@wille-zone.de>
2023-02-21 14:21:08 +0100

    chore: Add Example Interceptor

--- src/grpc/host/Program.cs
+++ src/grpc/host/Program.cs
@@ -1,13 +1,33 @@
+using Grpc.Core;
+using Grpc.Core.Interceptors;
 using ProtoBuf.Grpc.Server;
 using Service;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCodeFirstGrpc();
-builder.Services.AddCodeFirstGrpcReflection();
+builder.Services
+    .AddCodeFirstGrpc(_ => { })
+    .AddServiceOptions<GreeterService>(options => options.Interceptors.Add<LoggingInterceptor>());
 
 var app = builder.Build();
-
 app.MapGrpcService<GreeterService>();
-app.MapCodeFirstGrpcReflectionService();
-
 app.Run();
+
+
+public class LoggingInterceptor : Interceptor
+{
+    private readonly ILogger<LoggingInterceptor> _logger;
+
+    public LoggingInterceptor(ILogger<LoggingInterceptor> logger)
+    {
+        _logger = logger;
+    }
+    
+    public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(TRequest request, ServerCallContext context,
+        UnaryServerMethod<TRequest, TResponse> continuation)
+    {
+        _logger.LogDebug("Calling gRPC Method {method} with argument {argument}", context.Method, request);
+        var result = await base.UnaryServerHandler(request, context, continuation);
+        _logger.LogDebug("Called gRPC Method {method} with result {result}", context.Method, result);
+        return result;
+    }
+}
